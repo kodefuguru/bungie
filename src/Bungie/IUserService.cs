faee3578fd1439cb4a17a6a2d34f7193265ec50a
kodefuguru <kodefuguru@live.com>
2016-01-12 13:56:05 -0500

    UserService laid out and ready to determine dtos.

---
+++ src/Bungie/IUserService.cs
@@ -0,0 +1,14 @@
+﻿namespace Bungie
+{
+    using System.Threading.Tasks;
+
+    public interface IUserService
+    {
+        Task<object> GetAvailableThemes();
+        Task<object> GetAccount(long membershipId, MembershipType membershipType);
+        Task<object> GetUser(long membershipId);
+        Task<object> GetUserAliases(long membershipId);
+        Task<object> SearchUsers(string search);
+        Task<object> SearchUsers(string search, int page);
+    }
+}
---
+++ src/Bungie/UserService.cs
@@ -0,0 +1,77 @@
+﻿namespace Bungie
+{
+    using System.Threading.Tasks;
+    using BaseJump;
+
+    [Route("https://www.bungie.net/platform/user")]
+    public class UserService : BungieService
+    {
+        public UserService(string apiKey) : base(apiKey)
+        {
+        }
+
+        [Route("GetAvailableThemes")]
+        public Task<object> GetAvailableThemes()
+        {
+            return Request<object>();
+        }
+
+        [Route("GetBungieAccount/{membershipId}/{membershipType}")]
+        public Task<object> GetAccount(long membershipId, MembershipType membershipType) 
+        {
+            var model = new
+            {
+                membershipId,
+                membershipType
+            };
+            
+            return Request<object>(model); 
+        }
+
+        [Route("GetBungieNetUserById/{membershipId}")]
+        public Task<object> GetUser(long membershipId) 
+        {
+            var model = new
+            {
+                membershipId
+            };
+
+            return Request<object>(model); 
+        }
+
+        [Route("GetUserAliases/{membershipId}")]
+        public Task<object> GetUserAliases(long membershipId) 
+        {
+            var model = new
+            {
+                membershipId
+            };
+
+            return Request<object>(model); 
+        }
+
+
+        [Route("SearchUsers")]
+        public Task<object> SearchUsers(string search) 
+        {
+            var model = new
+            {
+                search
+            };
+
+            return Request<object>(model); 
+        }
+
+        [Route("SearchUsersPaged/{search}/page")]
+        public Task<object> SearchUsers(string search, int page)
+        {
+            var model = new
+            {
+                search,
+                page
+            };
+
+            return Request<object>(model);
+        }
+    }
+}
